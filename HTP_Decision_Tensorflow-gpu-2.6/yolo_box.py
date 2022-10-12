92a800b0dde71cadaa6098706feef7da849652e3
pandada <viej0102@gmail.com>
2022-10-12 19:42:31 +0900

    221012 HTP yolo 모델 box 크기 수정

--- HTP_Decision_Tensorflow-gpu-2.6/yolo_box.py
+++ HTP_Decision_Tensorflow-gpu-2.6/yolo_box.py
@@ -80,13 +80,13 @@ if __name__ == '__main__':
     # cv2.destroyAllWindows()
 
 
-    file_name = 'HTP_tree_75.png'
+    file_name = 'HTP_house_63.png'
     classes = []
     f = open('classes.txt', 'r')
     classes = [line.strip() for line in f.readlines()]
     colors = np.random.uniform(0, 255, size=(len(classes), 3))
 
-    img = cv2.imread(f"data/HTP_tree/{file_name}")
+    img = cv2.imread(f"data/HTP_house/{file_name}")
     ##### test 이미지 전처리 하기 #####
 
     # cv2.createCLAHE 균일화
@@ -152,11 +152,11 @@ if __name__ == '__main__':
     # box 크기가 a4용지 크기보다 커지는걸 방지
     if x < 0 : x = 0;
     if y < 0 : y = 0;
-    if x+w > 595 : w = 595-x;
-    if y+h > 842 : h = 842-y;
+    if x+w > width : w = width-x;
+    if y+h > height : h = height-y;
     print(x, y, w, h)
 
-    img_test = cv2.imread(f"data/HTP_tree/{file_name}")
+    img_test = cv2.imread(f"data/HTP_house/{file_name}")
     height, width, t_channels = img_test.shape
     print(height, width)
     # cv2.circle(img_test, (50, 70), 5, (255, 0, 255), 6)
