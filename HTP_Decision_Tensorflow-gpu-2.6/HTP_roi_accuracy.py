d232dbe28111c87976f7ca23dae1fb9b489a522c
pandada <viej0102@gmail.com>
2022-11-14 11:24:32 +0900

    221114 accuracy 그래프 만들기

--- HTP_Decision_Tensorflow-gpu-2.6/HTP_roi_accuracy.py
+++ HTP_Decision_Tensorflow-gpu-2.6/HTP_roi_accuracy.py
@@ -1,6 +1,8 @@
 import os
 import json
 import cv2
+import matplotlib.pyplot as plt
+import numpy as np
 
 def gt_read(file_name):
     with open(f'{gt_path}{file_name}', 'r') as f:
@@ -51,70 +53,143 @@ def IoU(box1, box2):
 
 
 if __name__ == '__main__':
-    list_name = ['house', 'tree', 'person']
-    result_list = []
-    for name in list_name:
-        gt_path = f"./data/HTP_paper/{name}_answer/"
-        gt_box = os.listdir(gt_path)
-        house_gt = [file for file in gt_box if file.endswith(".json")]
-        house_img = [file for file in gt_box if file.endswith(".JPG")]
+    confidence_list = []
+    F1_score_list = []
+    Accuracy_list = []
+    for confidence_threshold in range(1, 10):
+        list_name = ['house', 'tree', 'person']
+        result_list = []
+        for name in list_name:
+            gt_path = f"./data/HTP_paper/{name}_answer/"
+            gt_box = os.listdir(gt_path)
+            house_gt = [file for file in gt_box if file.endswith(".json")]
+            house_img = [file for file in gt_box if file.endswith(".JPG")]
 
-        pred_path = f"./data/HTP_paper/{name}_ver3_best/"
-        pred_box = os.listdir(pred_path)
-        house_pred = [file for file in pred_box if file.endswith(".txt")]
+            pred_path = f"./data/HTP_paper/{name}_ver3_best/"
+            pred_box = os.listdir(pred_path)
+            house_pred = [file for file in pred_box if file.endswith(".txt")]
 
-        # print(pred_read(house_pred[0]))
-        # print(gt_read(house_gt[0]))
-        #
-        # box1 = pred_read(house_pred[0])
-        # box1 = (box1[2], box1[3], box1[2]+box1[4], box1[3]+box1[5])
-        # box2 = gt_read(house_gt[0])
-        # box2 = (box2[0], box2[1], box2[0]+box2[2], box2[1]+box2[3])
+            # print(pred_read(house_pred[0]))
+            # print(gt_read(house_gt[0]))
+            #
+            # box1 = pred_read(house_pred[0])
+            # box1 = (box1[2], box1[3], box1[2]+box1[4], box1[3]+box1[5])
+            # box2 = gt_read(house_gt[0])
+            # box2 = (box2[0], box2[1], box2[0]+box2[2], box2[1]+box2[3])
 
-        TP = 0 # 옳은 검출
-        FN = 0 # 검출되어야 할 것이 검출되지 않았음
-        FP = 0 # 틀린 검출
-        TN = 0 # 검출되지 말아야 할 것이 검출되지 않았음
-        for i in range(len(house_gt)):
-            gt_r = gt_read(house_gt[i])
-            pred_r = pred_read(house_pred[i])
+            TP = 0 # 옳은 검출
+            FN = 0 # 검출되어야 할 것이 검출되지 않았음
+            FP = 0 # 틀린 검출
+            TN = 0 # 검출되지 말아야 할 것이 검출되지 않았음
+            for i in range(len(house_gt)):
+                gt_r = gt_read(house_gt[i])
+                pred_r = pred_read(house_pred[i])
 
-            if pred_r == None:
-                print(f"{house_pred[i]}는 객체 찾기 실패")
-                FN += 1
-                continue
-
-            confidence_threshold = 0.5
-
-            if pred_r[1] >= confidence_threshold:
-                box1 = (gt_r[1], gt_r[2], gt_r[1] + gt_r[3], gt_r[2] + gt_r[4])
-                box2 = (pred_r[2], pred_r[3], pred_r[2]+pred_r[4], pred_r[3]+pred_r[5])
-
-                if gt_r[0] != pred_r[0]:
-                    print("객체는 찾았으나 잘못 찾았음")
-                    FP += 1
+                if pred_r == None:
+                    print(f"{house_pred[i]}는 객체 찾기 실패")
+                    FN += 1
                     continue
 
-                iou = round(IoU(box1, box2), 2)
+                if pred_r[1] >= round(confidence_threshold * 0.1, 1):
+                    box1 = (gt_r[1], gt_r[2], gt_r[1] + gt_r[3], gt_r[2] + gt_r[4])
+                    box2 = (pred_r[2], pred_r[3], pred_r[2]+pred_r[4], pred_r[3]+pred_r[5])
 
-                if iou >= 0.5:
-                    print(f'{house_pred[i]}의 iou는 {iou*100}%')
-                    TP +=1
-                else:
-                    print(f'{house_pred[i]}는 객체는 찾았으나 정확도가 떨어짐. iou는 {iou*100}%')
-                    FN += 1
-            else: FN += 1
+                    if gt_r[0] != pred_r[0]:
+                        print("객체는 찾았으나 잘못 찾았음")
+                        FP += 1
+                        continue
 
-        Precision = round(TP / (TP+FP), 2) # 검출된 애들중에서 맞는애들, 오검출
-        Recall = round(TP / (TP+FN), 2) # 맞췄어야하는 애들중에서 참으로 맞춘애들, 미검출
-        Accuracy = round((TP+TN)/(TP+TN+FP+FN), 2)
-        F1_score = round(2*(Recall*Precision)/(Recall+Precision), 2)
-        result_list.append(f'{name} 전제 {len(house_gt)}개에서 TP는 {TP}, FP은 {FP}, FN는 {FN}, 정밀도는 {Precision}, 재현율는 {Recall}, Accuracy는 {Accuracy}, F1_score는 {F1_score}')
-    print(result_list)
+                    iou = round(IoU(box1, box2), 2)
 
+                    if iou >= 0.5:
+                        print(f'{house_pred[i]}의 iou는 {iou*100}%')
+                        TP +=1
+                    else:
+                        print(f'{house_pred[i]}는 객체는 찾았으나 정확도가 떨어짐. iou는 {iou*100}%')
+                        FN += 1
+                else: FN += 1
 
+            Precision = round(TP / (TP+FP), 2) # 검출된 애들중에서 맞는애들, 오검출
+            Recall = round(TP / (TP+FN), 2) # 맞췄어야하는 애들중에서 참으로 맞춘애들, 미검출
+            Accuracy = round((TP+TN)/(TP+TN+FP+FN), 2)
+            F1_score = round(2*(Recall*Precision)/(Recall+Precision), 2)
+            Accuracy_list.append(Accuracy)
+            F1_score_list.append(F1_score)
+            result_list.append(f'{name} 전제 {len(house_gt)}개에서 TP는 {TP}, FP은 {FP}, FN는 {FN}, 정밀도는 {Precision}, 재현율는 {Recall}, Accuracy는 {Accuracy}, F1_score는 {F1_score}')
+        print(result_list)
+        confidence_list.append(round(confidence_threshold*0.1, 1))
+    print(confidence_list)
+    print(Accuracy_list)
+    print(F1_score_list)
 
+    Accuracy_mena_list = []
+    Accuracy_mena_list.append(round(np.mean(Accuracy_list[:3]), 2))
+    Accuracy_mena_list.append(round(np.mean(Accuracy_list[3:6]), 2))
+    Accuracy_mena_list.append(round(np.mean(Accuracy_list[6:9]), 2))
+    Accuracy_mena_list.append(round(np.mean(Accuracy_list[9:12]), 2))
+    Accuracy_mena_list.append(round(np.mean(Accuracy_list[12:15]), 2))
+    Accuracy_mena_list.append(round(np.mean(Accuracy_list[15:18]), 2))
+    Accuracy_mena_list.append(round(np.mean(Accuracy_list[18:21]), 2))
+    Accuracy_mena_list.append(round(np.mean(Accuracy_list[21:24]), 2))
+    Accuracy_mena_list.append(round(np.mean(Accuracy_list[24:]), 2))
+    print(Accuracy_mena_list)
 
+    F1_score_mean_list = []
+    F1_score_mean_list.append(round(np.mean(F1_score_list[:3]), 2))
+    F1_score_mean_list.append(round(np.mean(F1_score_list[3:6]), 2))
+    F1_score_mean_list.append(round(np.mean(F1_score_list[6:9]), 2))
+    F1_score_mean_list.append(round(np.mean(F1_score_list[9:12]), 2))
+    F1_score_mean_list.append(round(np.mean(F1_score_list[12:15]), 2))
+    F1_score_mean_list.append(round(np.mean(F1_score_list[15:18]), 2))
+    F1_score_mean_list.append(round(np.mean(F1_score_list[18:21]), 2))
+    F1_score_mean_list.append(round(np.mean(F1_score_list[21:24]), 2))
+    F1_score_mean_list.append(round(np.mean(F1_score_list[24:]), 2))
+    print(F1_score_mean_list)
+
+    f, ax = plt.subplots(1, 2, figsize=(12, 8))
+    ax[0].plot(confidence_list, Accuracy_mena_list, 'go-')
+    ax[0].set_xlabel('confidence threshold')
+    ax[0].set_title('Accuracy', fontsize=20)
+    ax[0].set_xticks([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
+    ax[0].set_yticks([0.7, 0.75, 0.8, 0.85, 0.9, 0.95])
+    for i in range(len(confidence_list)):
+        height = Accuracy_mena_list[i]
+        ax[0].text(confidence_list[i], height - 0.0085, '%.2f' %height, ha='center', va='bottom', size=12)
+
+    ax[1].plot(confidence_list, F1_score_mean_list, 'bo-')
+    ax[1].set_xlabel('confidence threshold')
+    ax[1].set_title('F1_score', fontsize=20)
+    ax[1].set_xticks([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
+    ax[1].set_yticks([0.7, 0.75, 0.8, 0.85, 0.9, 0.95])
+    for i in range(len(confidence_list)):
+        height = F1_score_mean_list[i]
+        ax[1].text(confidence_list[i], height - 0.0085, '%.2f' %height, ha='center', va='bottom', size=12)
+
+    plt.show()
+"""
+    plt.figure(figsize=(10, 7))
+    plt.subplot(1, 2, 1)
+    plt.plot(confidence_list, Accuracy_mena_list, 'go-')
+    plt.xlabel('confidence threshold')
+    plt.title('Accuracy')
+    plt.xticks([0.1, 0.2, 0.3, 0.4, 0.5, 0.6 ,0.7 ,0.8 ,0.9])
+    plt.yticks([0.7, 0.75, 0.8, 0.85, 0.9, 0.95])
+    for i in range(len(confidence_list)):
+        height = Accuracy_mena_list[i]
+        plt.text(confidence_list[i], height - 0.25, '%.2f' %height, ha='center', va='bottom', size=12)
+
+    plt.subplot(1, 2, 2)
+    plt.plot(confidence_list, F1_score_mean_list, 'bo-')
+    plt.xlabel('confidence threshold')
+    plt.title('F1_score')
+    plt.xticks([0.1, 0.2, 0.3, 0.4, 0.5, 0.6 ,0.7 ,0.8 ,0.9])
+    plt.yticks([0.7, 0.75, 0.8, 0.85, 0.9, 0.95])
+    for i in range(len(confidence_list)):
+        height = F1_score_mean_list[i]
+        plt.text(confidence_list[i], height - 0.25, '%.2f' %height, ha='center', va='bottom', size=12)
+
+    plt.show()
+"""
     # answer이 잘 label 되었는지 확인해 보았습니다
     # with open(f'{gt_path}{house_gt[57]}', 'r') as f:
     #     json_data = json.load(f)
